ef3d599325b6d6105909ee5023fdf2e6b3c3cb98
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2019-02-20 22:39:04 +0900

    Fix WebView

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { WebView } from 'react-native'
 import styled from 'styled-components'
 
 const Wrap = styled.View`
-  flex: 1;
+  height: 500px;
 `
 
 const MyWebView = styled.WebView`
@@ -31,6 +31,8 @@ export default class App extends React.Component {
   _handleAppStateChange = nextAppState =>
     this.setState({ isForeground: nextAppState === 'active' })
 
+  onNavigationStateChange = navigator => console.log(navigator)
+
   render() {
     const { isForeground } = this.state
     return (
@@ -38,14 +40,14 @@ export default class App extends React.Component {
         {isForeground ? (
           <MyWebView
             source={{
-              html: `
-                      <html>
-                        <iframe width="100%" height="800" src="https://www.youtube.com/embed/v8y5qgWllT8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                      </html>
-                    `
+              uri:
+                'https://www.youtube.com/embed/v8y5qgWllT8?rel=0&autoplay=0&showinfo=0&controls=0'
             }}
-            scrollEnabled={false}
-            javaScriptEnabled={true}
+            onNavigationStateChange={this.onNavigationStateChange}
+            startInLoadingState
+            scalesPageToFit
+            javaScriptEnabled
+            style={{ flex: 1 }}
           />
         ) : null}
       </Wrap>
